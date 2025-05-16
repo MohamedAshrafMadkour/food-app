57cd3ebdfb3fde38b47d79affa0813ee0e8f0dc0
Mohamed Madkour <madkourmohamed88@gmail.com>
2025-05-16 10:44:33 +0300

    add images and fonts

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesDash
+  /// assets/images/dash.png
+  static const String imagesDash = "assets/images/dash.png";
+
+  /// Assets for imagesFavourite
+  /// assets/images/favourite.svg
+  static const String imagesFavourite = "assets/images/favourite.svg";
+
+  /// Assets for imagesFirst
+  /// assets/images/first.jpg
+  static const String imagesFirst = "assets/images/first.jpg";
+
+  /// Assets for imagesHome
+  /// assets/images/home.svg
+  static const String imagesHome = "assets/images/home.svg";
+
+  /// Assets for imagesIllustration
+  /// assets/images/Illustration.png
+  static const String imagesIllustration = "assets/images/Illustration.png";
+
+  /// Assets for imagesLogin1
+  /// assets/images/login1.png
+  static const String imagesLogin1 = "assets/images/login1.png";
+}
+
